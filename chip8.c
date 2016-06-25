dcf207109c1e8110f09e88311b95af516fc09dd9
Danila Fedorin <danila.fedorin@gmail.com>
2016-06-25 15:38:07 -0700

    Implement non-input-related operations.

--- chip8.c
+++ chip8.c
@@ -1,6 +1,7 @@
 #include "chip8.h"
 #include <stdio.h>
 #include <glfw/glfw3.h>
+#include <stdlib.h>
 
 void chipInit(chip_t* chip) {
   chip->pc = 0x200;
@@ -8,6 +9,7 @@ void chipInit(chip_t* chip) {
   chip->stackp = 0;
   chip->delay_timer = 0;
   chip->sound_timer = 0;
+
   for(int i = 0; i <   16; i++) chip->v[i] = 0;
   for(int i = 0; i < 4096; i++) chip->memory[i] = 0;
   for(int i = 0; i <   16; i++) chip->stack[i] = 0;
@@ -41,6 +43,131 @@ void chipClear(chip_t* chip){
 void chipStep(chip_t* chip) {
   unsigned short int opcode = (chip->memory[chip->pc] << 8) | (chip->memory[chip->pc + 1]);
   unsigned char head = ((opcode & 0xf000) >> 12) & 0xff;
+
+  if(head == 0x0){
+    unsigned char tail = opcode & 0xf;
+    if(tail == 0x0) {
+      chipClear(chip);
+    }
+    else if(tail == 0xe){
+      chip->pc = chip->stack[--(chip->stackp)];
+    }
+  } else if(head == 0x1) {
+    unsigned short int addr = opcode & 0xfff;
+    chip->pc = addr - 2;
+  } else if(head == 0x2) {
+    unsigned short int addr = opcode & 0xfff;
+    chip->stack[(chip->stackp)++] = chip->pc;
+    chip->pc = addr - 2;
+  } else if(head == 0x3) {
+    unsigned char reg = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char nn = opcode & 0xff;
+    if(chip->v[reg] == nn) (chip->pc) += 2;
+  } else if(head == 0x4) {
+    unsigned char reg = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char nn = opcode & 0xff;
+    if(chip->v[reg] != nn) (chip->pc) += 2;
+  } else if(head == 0x5) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char regy = ((opcode & 0xf0 ) >> 4) & 0xff;
+    if(chip->v[regx] == chip->v[regy]) (chip->pc) += 2;
+  } else if(head == 0x6) {
+    unsigned char reg = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char nn = opcode & 0xff;
+    chip->v[reg] = nn;
+  } else if(head == 0x7) {
+    unsigned char reg = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char nn = opcode & 0xff;
+    chip->v[reg] += nn;
+  } else if(head == 0x8) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char regy = ((opcode & 0xf0 ) >> 4) & 0xff;
+    unsigned char tail = opcode & 0xf;
+    if(tail == 0x0) chip->v[regx] = chip->v[regy];
+    else if(tail == 0x1) chip->v[regx] |= chip->v[regy];
+    else if(tail == 0x2) chip->v[regx] &= chip->v[regy];
+    else if(tail == 0x3) chip->v[regx] ^= chip->v[regy];
+    else if(tail == 0x4) {
+      unsigned short int result = chip->v[regx] + chip->v[regy];
+      chip->v[0xf] = !((result & 0xff00) == 0);
+      chip->v[regx] = result & 0xff;
+    } else if(tail == 0x5) {
+      unsigned short int result = chip->v[regx] - chip->v[regy];
+      chip->v[0xf] = ((result & 0xff00) == 0);
+      chip->v[regx] = result & 0xff;
+    } else if(tail == 0x6) {
+      chip->v[0xf] = chip->v[regx] & 0x1;
+      chip->v[regx] = chip->v[regx] >> 1;
+    } else if(tail == 0x7) {
+      unsigned short int result = chip->v[regy] - chip->v[regx];
+      chip->v[0xf] = ((result & 0xff00) == 0);
+      chip->v[regx] = result & 0xff;
+    } else if(tail == 0xe) {
+      chip->v[0xf] = chip->v[regx] & 0x80;
+      chip->v[regx] = (chip->v[regx] << 1) & 0xff;
+    }
+  } else if(head == 0x9) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char regy = ((opcode & 0xf0 ) >> 4) & 0xff;
+    if(chip->v[regx] != chip->v[regy]) (chip->pc) += 2;
+  } else if(head == 0xA) {
+    chip->i = opcode & 0xfff;
+  } else if(head == 0xB) {
+    chip->pc = chip->i + chip->v[0];
+  } else if(head == 0xC) {
+    unsigned char random = rand() % 0xff;
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char nn = opcode & 0xff;
+    chip->v[regx] = random & nn;
+  } else if(head == 0xD) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char regy = ((opcode & 0xf0 ) >> 4) & 0xff;
+    unsigned char height = opcode & 0xf;
+    chip->v[0xf] = 0;
+    for(int y = 0; y < height; y++){
+      unsigned char sprline = chip->memory[chip->i + y];
+      for(int x = 0; x < 8; x++){
+        unsigned char pixel = sprline & (0x80 >> x);
+        if(pixel){
+          unsigned char drawx = (chip->v[regx] + x) % 64;
+          unsigned char drawy = (chip->v[regy] + y) % 32;
+          unsigned short int disp = drawx + drawy * 64;
+          if(chip->display[disp]) chip->v[0xf] = 1;
+          chip->display[disp] ^= 1;
+        }
+      }
+    }
+  } else if(head == 0xE) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+  } else if(head == 0xF) {
+    unsigned char regx = ((opcode & 0xf00) >> 8) & 0xff;
+    unsigned char tail = opcode & 0xff;
+    if(tail == 0x07) chip->v[regx] = chip->delay_timer;
+    else if(tail == 0x0a) {
+
+    }
+    else if(tail == 0x15) chip->delay_timer = chip->v[regx];
+    else if(tail == 0x18) chip->sound_timer = chip->v[regx];
+    else if(tail == 0x1e) chip->i += chip->v[regx];
+    else if(tail == 0x29) {
+      chip->i = 0x50 + (chip->v[regx] * 5);
+    }
+    else if(tail == 0x33) {
+      unsigned char value = chip->v[regx];
+      unsigned char units = value % 10;
+      unsigned char tens =  (value % 100) / 10;
+      unsigned char hundreds = value / 100;
+      chip->memory[chip->i] = hundreds;
+      chip->memory[chip->i + 1] = tens;
+      chip->memory[chip->i + 2] = units;
+    }
+    else if(tail == 0x55) {
+      for(int i = 0; i <= regx; i++) chip->memory[chip->i + i] = chip->v[i];
+    }
+    else if(tail == 0x65) {
+      for(int i = 0; i <= regx; i++) chip->v[i] = chip->memory[chip->i + i];
+    }
+  }
 
   (chip->pc) += 2;
 }
